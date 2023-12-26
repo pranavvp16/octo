d810cb363e69b4f809e73b773ceb7a23b65b8ef1
Vedant Agrawal <vrsagrawal@gmail.com>
2023-12-26 14:25:09 +0530

    Added email password authentication

--- octo/cli.py
+++ octo/cli.py
@@ -1,6 +1,11 @@
 import argparse
 from constants.info import DESCRIPTION, VERSION, ASCII_ART
 import os
+import keyring
+
+def save_password(email, password):
+    # Set the password in the keyring
+    keyring.set_password(service_name="Octo", username=email, password=password)
 
 parser = argparse.ArgumentParser(
     description=DESCRIPTION,
@@ -16,7 +21,7 @@ parser.add_argument(
 )
 parser.add_argument(
     'auth',
-    help = "Authenticate the user with Mindsdb",
+    help="Authenticate the user with Mindsdb",
     action='store_true'
 )
 
@@ -26,8 +31,17 @@ parser.add_argument(
 )
 
 args = parser.parse_args()
+
 if args.auth:
     email = input("Enter your email: ")
     password = input("Enter your password: ")
-    print("Authenticating user")
+    
+    # Save the password using keyring
+    save_password(email, password)
+    print(f"Password successfully set for {email}")
+    # You can add additional authentication logic here if needed
+
+elif args.init:
+    # Add logic for project initialization here
+    print("Initializing the project")
 
